7240989bd17bcca0c6082051de8cb3db4f13312c
jun <ggprgrkjh@naver.com>
2023-10-25 20:53:48 +0900

    feat: split 메소드 text == null 리턴 빈 문자열

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -2,6 +2,10 @@ package main;
 
 public class StringCalculator {
     public String[] split(String text) {
+        if(text == null){
+            return new String[]{};
+        }
+
         return text.split(",|\n");
     }
 }
