960fb7c791328709c2fc7c20fff4eefdef64fdfe
jun <ggprgrkjh@naver.com>
2023-10-25 20:51:58 +0900

    feat: testSplitWhenComma 메소드

--- src/test/StringCalculatorTest.java
+++ src/test/StringCalculatorTest.java
@@ -2,11 +2,23 @@ import main.StringCalculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class StringCalculatorTest {
+public class StringCalculatorTest  {
+
+    private StringCalculator cal;
+
+    StringCalculatorTest(){
+        cal = new StringCalculator();
+    }
 
     @Test
     public void testSplit() throws Exception {
-        StringCalculator cal = new StringCalculator();
+        String[] result = cal.split("1");
+        Assertions.assertArrayEquals(new String[]{"1"}, result);
+    }
+
+
+    @Test
+    public void testSplitWhenComma() throws Exception {
         String[] result = cal.split("1,2,3");
         Assertions.assertArrayEquals(new String[]{"1", "2", "3"}, result);
     }
@@ -14,7 +26,6 @@ public class StringCalculatorTest {
 
     @Test
     public void testSplitWhenNewLine() throws Exception {
-        StringCalculator cal = new StringCalculator();
         String[] result = cal.split("1\n2\n3");
         Assertions.assertArrayEquals(new String[]{"1", "2", "3"}, result);
     }
