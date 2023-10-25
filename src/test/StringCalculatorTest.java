e118f5ca76c84f1d98f1150741e2a457fc0e2ac8
jun <ggprgrkjh@naver.com>
2023-10-25 20:38:03 +0900

    feat: StringCalculator 생성

--- src/test/StringCalculatorTest.java
+++ src/test/StringCalculatorTest.java
@@ -1,6 +1,13 @@
+import main.StringCalculator;
+import org.junit.jupiter.api.Assertions;
+
 public class StringCalculatorTest {
 
-    public void testSplit() throws Exception{
+    public void testSplit() throws Exception {
+        StringCalculator cal = new StringCalculator();
         String[] result = split("1,2,3");
+        Assertions.assertArrayEquals(new String[]{"1", "2", "3"}, result);
     }
+
+
 }
